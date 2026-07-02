b9d5b711348fafe368ddacf2deb83e57bae92bb9
besnovatyj <metr31@yandex.ru>
2026-07-02 21:52:06 +0500

    extract modules contracts

--- src/Module.php
+++ src/Module.php
@@ -7,12 +7,12 @@
 namespace Besnovatyj\Catalog;
 
 use common\components\module\CmsModule;
-use modules\modman\contract\DeclaresModule;
-use modules\modman\contract\ProvidesAdminMenu;
-use modules\modman\contract\ProvidesDependencies;
-use modules\modman\contract\ProvidesDirectories;
-use modules\modman\contract\ProvidesMigrations;
-use modules\modman\contract\ProvidesOptions;
+use Besnovatyj\Contracts\module\DeclaresModule;
+use Besnovatyj\Contracts\module\ProvidesAdminMenu;
+use Besnovatyj\Contracts\module\ProvidesDependencies;
+use Besnovatyj\Contracts\module\ProvidesDirectories;
+use Besnovatyj\Contracts\module\ProvidesMigrations;
+use Besnovatyj\Contracts\module\ProvidesOptions;
 
 class Module  extends CmsModule implements
     DeclaresModule, ProvidesAdminMenu,
